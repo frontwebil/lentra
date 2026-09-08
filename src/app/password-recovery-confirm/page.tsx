847207c7810979b2a8c057bd17628abf5ede68f2
Illya Sukhomlyn <gollpfd@gmail.com>
2026-09-09 00:21:41 +0300

    update

--- src/app/password-recovery-confirm/page.tsx
+++ src/app/password-recovery-confirm/page.tsx
@@ -1,9 +1,11 @@
 import { ConfirmPassword } from "@/Components/AuthPage/ConfirmPassword/ConfirmPassword";
+import { LoginHeader } from "@/Components/AuthPage/LoginHeader/LoginHeader";
 import { Suspense } from "react";
 
 export default function RecoveryConfirmPassword() {
   return (
     <Suspense fallback={null}>
+      <LoginHeader />
       <ConfirmPassword />
     </Suspense>
   );
